db0ab67c1840e74a7f6edaff3e67f38bd4d4fd1b
j3nka94 <jens.johansson94@gmail.com>
2018-05-30 21:35:13 +0200

    Added Base Stats

--- MOBA Game/Assets/Scripts/Characters/BaseCharacter.cs
+++ MOBA Game/Assets/Scripts/Characters/BaseCharacter.cs
@@ -4,9 +4,27 @@
 public class BaseCharacter : ScriptableObject {
 
     [SerializeField] protected string characterName;
+    [SerializeField] protected string description;
 
+    // In game variables
     [SerializeField] protected int health;
     [SerializeField] protected float armor, magicArmor;
+    [SerializeField] protected float attackRange;
+    [SerializeField] protected float movementSpeed;
+    [SerializeField] protected AttackType attackType = AttackType.PHYSICAL;
+    [SerializeField] protected AttackRangeType attackRangeType = AttackRangeType.MELEE;
+}
 
+public enum AttackType
+{
+    PHYSICAL,
+    MAGICAL,
+    TRUE,
+    MIXED
+}
 
+public enum AttackRangeType
+{
+    MELEE,
+    RANGED
 }
