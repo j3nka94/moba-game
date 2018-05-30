1d4510e498cb7f6475f2f0fc78954c860352c985
j3nka94 <jens.johansson94@gmail.com>
2018-05-30 19:41:43 +0200

    test

---
+++ MOBA Game/Assets/Scripts/Movement/MovementInput.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class MovementInput : MonoBehaviour {
+
+    // How fast the player should move. (Later take that value from player stats)
+    [SerializeField] float movementSpeed;
+
+    // How fast the player should rotate towards the target.
+    [SerializeField] float rotationSpeed;
+
+    // The layers the raycast will detect?
+    [SerializeField] LayerMask acceptableTarget;
+
+    // The player's camera.
+    [SerializeField] Camera playerCam;
+
+    // The NavMeshAgent attached to the player.
+    [SerializeField] NavMeshAgent playerAgent;
+
+    // The path given from the NavMesh.
+    List<Vector3> path;
+
+    NavMeshPath navMeshPath = new NavMeshPath();
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            GetTargetPosition(Input.mousePosition);
+        }
+
+        if (path != null)
+        {
+            UpdateMovement();
+        }
+    }
+
+    void GetTargetPosition(Vector3 mousePos)
+    {
+        // Raycast from mouse position.
+        Ray ray = playerCam.ScreenPointToRay(mousePos);
+        RaycastHit hit;
+        if(Physics.Raycast(ray, out hit, Mathf.Infinity, acceptableTarget))
+        {
+            path = navMeshPath.GetPath(hit.point, playerAgent);
+
+            if(path == null)
+            {
+                return;
+            }
+
+            // Path should be at player height at all time
+            for (int i = 0; i < path.Count; i++)
+            {
+                Vector3 currPos = path[i];
+                currPos.y = 5f;
+                path[i] = currPos;
+            }
+        }
+    }
+
+    void UpdateMovement()
+    {
+        Vector3 dir = (path[0] - transform.position).normalized;
+        transform.position += dir * movementSpeed * Time.deltaTime;
+    }
+}
---
+++ MOBA Game/Assets/Scripts/Movement/NavMeshPath.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NavMeshPath {
+
+    public List<Vector3> GetPath(Vector3 targetPos, NavMeshAgent agent)
+    {
+        agent.SetDestination(targetPos);
+
+        while (agent.pathPending)
+        {
+            //Wait for path
+        }
+
+        if (agent.hasPath)
+        {
+            // Convert from array to list
+            List<Vector3> positions = new List<Vector3>();
+            for (int i = 0; i < agent.path.corners.Length; i++)
+            {
+                positions.Add(agent.path.corners[i]);
+            }
+
+            return positions;
+        }
+
+        return null;
+    }
+}
