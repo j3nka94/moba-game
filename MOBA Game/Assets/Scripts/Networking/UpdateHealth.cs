29b8083b4c46083b5e6d8c3ef308db908f7b5377
j3nka94 <jens.johansson94@gmail.com>
2018-06-02 13:25:28 +0200

    adf

---
+++ MOBA Game/Assets/Scripts/Networking/UpdateHealth.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+
+public class UpdateHealth : NetworkBehaviour {
+
+    public float maxHealth;
+
+    [SyncVar(hook = "OnUpdateHealthBar")]
+    public float currentHealth;
+
+    public RectTransform healthBar;
+
+    public void OnUpdateMaxHealth(float amount)
+    {
+        maxHealth += amount;
+    }
+
+	public void OnUpdateHealth(float amount)
+    {
+        if (!isServer)
+            return;
+
+        currentHealth += amount;
+    }
+
+    void OnUpdateHealthBar(float health)
+    {
+        healthBar.sizeDelta = new Vector2(health, healthBar.sizeDelta.y);
+    }
+}
---
+++ MOBA Game/Assets/Scripts/Networking/UpdateTransform.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class UpdateTransform : NetworkBehaviour {
+
+    #region Singleton
+    public static UpdateTransform Instance { get; private set; }
+
+    private void Awake()
+    {
+        if(Instance  == null)
+        {
+            Instance = this;
+        }
+        else if(Instance != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+    #endregion
+
+    public void UpdatePosition(GameObject player, Vector3 position)
+    {
+
+    }
+}
